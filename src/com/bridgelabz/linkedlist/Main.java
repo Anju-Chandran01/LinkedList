aab2393b7d0e746b59a59de1fc0c04e0d52ffc7a
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-01 04:41:08 -0700

    Added : Created a new Linked List

---
+++ src/com/bridgelabz/linkedlist/Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("--- Welcome to Linked List programs ---");
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(30);
+        linkedList.add(56);
+        linkedList.add(70);
+
+        linkedList.display();
+    }
+}
